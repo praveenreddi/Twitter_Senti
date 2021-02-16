f73de9aa7d49a4faaf4309fc055ed2c61e40f65e
praveenreddi <30587635+praveenreddi@users.noreply.github.com>
2021-02-16 13:43:10 +0530

    Update configurations.py
    added Keys

--- Downloads/Tweenalyzer---The-Twitter-Analyzer-master/Tweenalyzer---The-Twitter-Analyzer-master/configurations.py
+++ Downloads/Tweenalyzer---The-Twitter-Analyzer-master/Tweenalyzer---The-Twitter-Analyzer-master/configurations.py
@@ -1,6 +1,6 @@
-consumer_key = ""
-consumer_secret = ""
-access_token = ""
-access_secret = ""
+consumer_key = "tzP5Ic3ugoswQ32hsdxSoQzvI"
+consumer_secret = "8mVtfbpVhnS5I9oJmHuKlpvyAGLmXMm1agEEjDHL76iNlw96o3"
+access_token = "860404387285434368-hrkPXPZsI0WtlIYnh4X9kpvLupzKvTX"
+access_secret = "ET7gpwE8mgjHXOgCNYyolbbMZsLmb96fWgoi7yYVOKgLM"
 
-google_maps_key = ''
+google_maps_key = 'AIzaSyBESAALaWbBdSBjKGE4MkVsgE3V0a4kr74'
